9cfafe1530e96fde316d9bcd45365a64ae84d567
Fred Young <fred@wanion.net>
2015-01-20 17:16:47 +1300

    Update PowerShell to be more idiomatic
    Reformat create-demo.ps1 to be more typical PowerShell style.
    Some tweaks to how it operates, such as passwords that meet
    complexity requirements, but should have no other functional changes.
    Remove new.ps1 script which duplicates create-demo.ps1.

--- bin/create_demo.ps1
+++ bin/create_demo.ps1
@@ -1,87 +1,111 @@
-Import-module ActiveDirectory
+#region Configuration
 
-# Crude Random Password Generator 
-Function GeneratePassword
-{
-    # How many Characters Minimum?
-    $Length=15
-    # Create a password choosing everying from Character 34 to 127
-    1..$Length | foreach{ $Password+=([char]((Get-Random 93)+34))}
-    # Convert to a Secure String
-    $Password=Convertto-SecureString $Password -asplaintext -force
-    Return $Password
+# Root OU below DomainRoot
+$RootOUName = "Marvel Small"
+#$RootOUName = "Marvel Medium"
+#$RootOUName = "Marvel Large"
+
+# DistinguishedName of Domain Root
+$DomainRoot = "DC=ava,DC=test,DC=domain"
+
+# Whatever the name of the $RootOUName Combined with Domain
+$CompanyRootDN = "OU=$RootOUName,$DomainRoot"
+
+# UPN Extension to the Domain
+$UPNSuffix = "@ava.test.domain"
+
+# List of Office Names
+$CityList = "Avengers Mansion","Rikers Island","Stark Tower","Sanctum Sanctorum","Ravencroft","Asgard"
+
+# List of Divisions Per Office
+$DivisionList = "IT","Sales","Marketing","HR","Architecture","Operations","Development","Strategy","Finance","Administration","Security","Logistics","Customer Services","Research & Development","Market Development","Business Development","Management","Engineering","Infrastructure"
+
+#endregion
+
+#region Main script
+
+Import-Module ActiveDirectory
+
+# Don't separate array elements when converting to strings (default is a space)
+$ofs = ''
+
+Function Get-CrudePassword {
+<#
+.SYNOPSIS
+Create a basic ASCII password.
+.DESCRIPTION
+A basic random password generator.
+It will create a string of printable ASCII characters of the length specified that meets typical complexity requirements.
+.PARAMETER Length
+Character length of password to generate.
+#>
+    Param(
+        [int]$Length = 15
+    )
+
+    do
+    {
+        $Password = [string](1..$Length | foreach { [Char](Get-Random -Min 32 -Max 126) })
+    } until # Lazy complexity requirement check
+        ($Password -cmatch '[A-Z]' -And
+        $Password -cmatch '[a-z]' -And
+        $Password -cmatch '\d')
+
+    # Return as Secure String
+    ConvertTo-SecureString $Password -AsPlainText -Force
 }
 
-Function GET-GroupInfo()
+Function Get-GroupInfo
 {
     Param($City,$Division)
-    $GroupName=$City.replace(" ","")+"-"+$Division.replace(" ","")
+    $GroupName = "$City-$Division" -Replace ' '
     $GroupDescription="$Division in $City Access Group"
-    # Return the Results (This is a feature new to version 3)
-    [pscustomobject]@{Name=$Groupname;Description=$GroupDescription}
-}
 
-# Define OU at Base of AD for Offices
-$BaseOU="Marvel Small"
-#$BaseOU="Marvel Medium"
-#$BaseOU="Marvel Large"
-# Provide List of Office Names
-$CityOU="Avengers Mansion","Rikers Island","Stark Tower","Sanctum Sanctorum","Ravencroft","Asgard"
-# Provide List of Divisions Per Office
-$DivisionOU="IT","Sales","Marketing","HR","Architecture","Operations","Development","Strategy","Finance","Administration","Security","Logistics","Customer Services","Research & Development","Market Development","Business Development","Management","Engineering","Infrastructure"
-# DistinguishedName of Domain Root
-$Domain="DC=ava,DC=test,DC=domain"
-# Whatever the name of the $BaseOU Combined with Domain
-$CompanyPath="OU=$BaseOU,"+$Domain
-# UPN Extension to the Domain
-$UPN="@ava.test.domain"
-# Create BaseOU for Offices
-NEW-ADOrganizationalUnit -name $BaseOU -path $Domain
-
-# Gather through list of Cities
-Foreach ($City in $CityOU) 
-{
-    # Create OU for City
-    NEW-ADOrganizationalUnit -path $CompanyPath -name $City
-    # Gather through list of Divisions
-    Foreach($Division in $DivisionOU)
-    {
-        # Create Division within City
-        NEW-ADOrganizationalUnit -path "OU=$City,$CompanyPath" -name $Division
-        # Create Group within Division and Description
-        $Groupdata=GET-Groupinfo –City $City –Division $Division
-        $GroupName=$Groupdate.Name
-        $GroupDescription=$Groupdata.Description
-        NEW-ADGroup -name $GroupName -GroupScope Global -Description $GroupDescription -Path "OU=$Division,OU=$City,$CompanyPath"
+    # Return as custom object
+    New-Object PSObject -Property @{
+        Name = $Groupname
+        Description = $GroupDescription
     }
 }
-# Pull together list of CSV raw data supplied from Generator
-# 
-$all_names=Get-Content C:\Script\marvel_names.txt
-# Generate 150 Random Users from pulled Raw data
-For ($x=0;$x -lt 150;$x++)
+
+# Create base OU for Offices
+New-ADOrganizationalUnit -name $RootOUName -path $DomainRoot
+
+# Create OUs for cities and divisions
+foreach ($City in $CityList) 
 {
-       $name=GET-Random $all_names.Name
-       $bits=$name.Split(" ")
-       $Firstname=$bits[0]
-       $Lastname=$bits[1]
-       $Displayname=$name
-          {
-            # Pick a Random City
-            $City=GET-RANDOM $Cityou
-            # Pick a Random Division
-            $Division=GET-RANDOM $DivisionOU
-            $LoginID=$Firstname.substring(0,1)+$Lastname
-            $UserPN=$LoginID+$UPN
-            $Sam=$LoginID.padright(20).substring(0,20).trim()
-            # Define their path in Active Directory
-            $ADPath="OU=$Division,OU=$City,$CompanyPath"
-            # Create the user in Active Directory
-            New-ADUser -GivenName $Givenname -Surname $Surname -DisplayName $Displayname -UserPrincipalName $UserPN -Division $Division -City $City -Path $ADPath -name $Displayname -SamAccountName $Sam –userpassword (GENERATEPASSWORD)
-            # Add User to appropriate Security Group
-            $Groupname=(GET-GroupInfo –city $City –division $Division).Name
-            ADD-ADGroupmember $Groupname –members $Sam
-            # Enable the account for access
-            ENABLE-ADAccount $Sam
-          }
- } 
+    New-ADOrganizationalUnit -path $CompanyRootDN -name $City
+    foreach ($Division in $DivisionList)
+    {
+        New-ADOrganizationalUnit -path "OU=$City,$CompanyRootDN" -name $Division
+
+        # Create division group
+        $GroupData = Get-GroupInfo –City $City –Division $Division
+        New-ADGroup -Name $GroupData.Name -GroupScope Global -Description $GroupData.Description -Path "OU=$Division,OU=$City,$CompanyRootDN"
+    }
+}
+
+# Select 150 random names from name list
+# Only select names with two parts, and without possessive on first part (to exclude "So-and-so's twin")
+$NameList = Get-Content names.txt | Where-Object { ($_ -Split ' ').Count -eq 2 -and ($_ -notmatch "^\S+'s ") } | Get-Random -Count 150
+
+# Generate users from name list
+foreach ($Name in $NameList) {
+    $Firstname,$Lastname = $Name.Split(" ")
+    $City = Get-Random $CityList
+    $Division = Get-Random $DivisionList
+
+    $LoginID = $Firstname[0] + $Lastname
+    $userPrincipalName = $LoginID + $UPNSuffix
+    $sAMAccountName = [string]$LoginID[0..19]
+
+    # Define their path in Active Directory
+    $ADPath = "OU=$Division,OU=$City,$CompanyRootDN"
+    # Create the user in Active Directory
+    New-ADUser -Enabled -GivenName $Firstname -Surname $Lastname -DisplayName $Displayname -UserPrincipalName $userPrincipalName -Division $Division -City $City -Path $ADPath -Name $Displayname -SamAccountName $sAMAccountName –UserPassword (Get-CrudePassword)
+    # Add User to appropriate Security Group
+    $Groupname = (Get-GroupInfo –City $City –Division $Division).Name
+    Add-ADGroupmember $Groupname –Members $sAMAccountName
+} 
+
+#endregion
