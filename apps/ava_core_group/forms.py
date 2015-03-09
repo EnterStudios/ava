c6f922a2165b3b67b37a7d8603a9f03adb224673
Laura Bell <laura@safestack.io>
2015-03-09 21:58:57 +1300

    Clean up of commented out code

--- apps/ava_core_group/forms.py
+++ apps/ava_core_group/forms.py
@@ -6,8 +6,6 @@ from apps.ava_core_group.models import Group, GroupType
 class GroupForm(ModelForm):
     class Meta:
         model = Group
-        #fields = ('name',  'description', 'group_type','parent')
-
 
 class GroupTypeForm(ModelForm):
     class Meta:
