f4f3d09ec94dad5e6f83d23989ab055e1511d3c3
Adam Couper <lokifer@gmail.com>
2015-04-07 21:54:11 +1200

    Created install-ansible.sh, modified Vagrantfile to call it as an inline shell script. Installs ansible on the vagrant machine and runs it locally

---
+++ vagrant/install-ansible.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [ $(dpkg-query -W -f='${Status}' ansible 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+	export DEBIAN_FRONTEND=noninteractive
+	echo "Adding Ansible PPA"
+    apt-add-repository ppa:ansible/ansible &> /dev/null || exit 1
+    echo "Ansible PPA installed"
+
+    echo "Updating APT"
+    apt-get update -qq &> /dev/null || exit 1
+    echo "APT Updated"
+
+    echo "Installing Ansible"
+    apt-get install -qq ansible &> /dev/null || exit 1
+    echo "Ansible installed"
+fi
