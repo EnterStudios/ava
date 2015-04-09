4cf3b7a1a16e2efe3d33a22b6dc3695e37ad78eb
Adam Couper <lokifer@gmail.com>
2015-04-09 20:30:15 +1200

    Changing copies + regex into templates. Have to have an extra step of copying due to weird windows host vboxfs permission/command issues

--- vagrant/install-ansible.sh
+++ vagrant/install-ansible.sh
@@ -3,16 +3,16 @@
 # Check if ansible is installed on the vagrant box first
 if [ $(dpkg-query -W -f='${Status}' ansible 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-	export DEBIAN_FRONTEND=noninteractive
-	echo "Adding Ansible PPA"
+#    export DEBIAN_FRONTEND=noninteractive
+    echo "Adding Ansible PPA to apt sources"
     apt-add-repository ppa:ansible/ansible &> /dev/null || exit 1
     echo "Ansible PPA installed"
 
-    echo "Updating APT"
+    echo "Updating apt"
     apt-get update -qq &> /dev/null || exit 1
-    echo "APT Updated"
+    echo "apt updated"
 
-    echo "Installing Ansible"
+    echo "Installing ansible"
     apt-get install -qq ansible &> /dev/null || exit 1
     echo "Ansible installed"
 fi
