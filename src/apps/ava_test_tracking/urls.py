13f731d9f8f09d5b1548291a0b91905876d30703
Laura Bell <ladynerd@Lauras-MacBook-Pro.local>
2015-06-22 10:25:23 +1200

    Pep8 clean up of aa_test_tracking

--- src/apps/ava_test_tracking/urls.py
+++ src/apps/ava_test_tracking/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
 from apps.ava_test_tracking import views
 
-
 urlpatterns = patterns('',
-    url(r'^e/(?P<token>[^/]+)/(?P<extra>.*)?$', views.RecordEmailTestResultView.as_view(), name='email-test-tracking'),
-    url(r'^t/(?P<token>[^/]+)/(?P<extra>.*)?$', views.RecordEmailTestResultView.as_view(), name='twitter-test-tracking'),
-)
+                       url(r'^e/(?P<token>[^/]+)/(?P<extra>.*)?$', views.RecordEmailTestResultView.as_view(),
+                           name='email-test-tracking'),
+                       url(r'^t/(?P<token>[^/]+)/(?P<extra>.*)?$', views.RecordEmailTestResultView.as_view(),
+                           name='twitter-test-tracking'),
+                       )
