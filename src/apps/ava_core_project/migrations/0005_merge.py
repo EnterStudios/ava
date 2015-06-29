f6490a79df12487c8bb4e5ed774c2e429a5da9c3
Laura Bell <ladynerd@Lauras-MacBook-Pro.local>
2015-06-29 19:20:28 +1200

    Merged migrations

---
+++ src/apps/ava_core_project/migrations/0005_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ava_core_project', '0004_auto_20150617_0125'),
+        ('ava_core_project', '0003_auto_20150618_2040'),
+    ]
+
+    operations = [
+    ]
