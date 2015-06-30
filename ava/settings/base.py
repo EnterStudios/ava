1bde094bc6e0b9991a63cb05f727c27857125a6a
Tom Eastman <tom@eastman.net.nz>
2015-06-30 17:11:11 +1200

    Configure use of Redis for sessions and caches

--- ava/settings/base.py
+++ ava/settings/base.py
@@ -144,11 +144,31 @@ class REDIS_DATABASES(enum.IntEnum):
 USE_REDIS_SESSIONS = True  # Turned off for the moment -- not needed.
 if USE_REDIS_SESSIONS:
     SESSION_ENGINE = 'redis_sessions.session'
-    SESSION_REDIS_HOST = os.environ.get('REDIS_PORT_6379_TCP_ADDR', None)
-    SESSION_REDIS_PORT = os.environ.get('REDIS_PORT_6379_TCP_PORT', None)
-    SESSION_REDIS_DB = REDIS_DATABASES.DJANGO_SESSION_FRAMEWORK
+    SESSION_REDIS_HOST = os.environ.get('REDIS_PORT_6379_TCP_ADDR', 'localhost')
+    SESSION_REDIS_PORT = os.environ.get('REDIS_PORT_6379_TCP_PORT', '6379')
+    SESSION_REDIS_DB = int(REDIS_DATABASES.DJANGO_SESSION_FRAMEWORK)
     SESSION_REDIS_PREFIX = 'session'
-   
+
+## Using redis for the cache would be nice, but the library is still buggy
+## with Python 3, When they fix the bugs hopefully we can just flip this
+## switch.
+USE_REDIS_CACHE = False  ## Disabled because of Python 3 compatibility bugs.
+if USE_REDIS_CACHE:
+    CACHES = {
+        'default': {
+            'BACKEND': 'redis_cache.RedisCache',
+            'LOCATION': (
+                '{}:{}'.format(
+                    os.environ.get('REDIS_PORT_6379_TCP_ADDR', 'localhost'),
+                    os.environ.get('REDIS_PORT_6379_TCP_PORT', '6379'),
+                )
+            ),
+            'OPTIONS': {
+                'DB': REDIS_DATABASES.DJANGO_CACHING_FRAMEWORK,
+            }
+        },
+    }
+    
 LOGIN_REDIRECT_URL= "/"
 
 PUBLIC_SITE_URLS = [
