e3dc720631a68bd8a9cc16dc3556330d56cb5491
Tom Eastman <tom@eastman.net.nz>
2015-07-03 14:18:50 +1200

    Implement a CSP policy

--- ava/settings/base.py
+++ ava/settings/base.py
@@ -63,6 +63,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'csp.middleware.CSPMiddleware',
 )
 
 ROOT_URLCONF = 'ava.urls'
@@ -223,4 +224,54 @@ CELERY_RESULT_BACKEND = 'redis://{}:{}/{}'.format(
     os.environ.get('REDIS_PORT_6379_TCP_ADDR', None),
     os.environ.get('REDIS_PORT_6379_TCP_PORT', None),
     REDIS_DATABASES.CELERY_RESULT_BACKEND
+)
+
+
+## CONTENT SECURITY POLICY (CSP) CONFIGURATION
+##
+## Here is where we configure exemptions from the default (strong)
+## CSP policy. In an ideal world, this will be empty.
+##
+## Documented at: http://django-csp.readthedocs.org/en/latest/configuration.html
+##
+## CSP crash course at: http://django-csp.readthedocs.org/en/latest/configuration.html
+
+CSP_STYLE_SRC = (
+    ## 'self' is for all local assets, the usual default.
+    "'self'",
+
+    ## Unfortunately 'modernizr' seems to require 'unsafe-inline' styles.
+    "'unsafe-inline'",
+
+    ## CDN for a stylesheet we're pulling in.
+    "http://maxcdn.bootstrapcdn.com/"
+)
+
+CSP_SCRIPT_SRC = (
+    "'self'",
+    ## JQuery is being pulled from a CDN.
+    "http://ajax.googleapis.com",
+
+    ## This sha256 excemption is for the tiny little bit of javascript
+    ## that the django-html5-boilerplate library is injecting at the
+    ## bottom of seemingly every page. It would be nice for this to go
+    ## away somehow.
+    "'sha256-QElWz9ZyO3cMaja_DMF2vK4SfsTklXYMigNFQGuxka8='",
+)
+
+CSP_FONT_SRC = (
+    "'self'",
+    ## Bootstrap CDN
+    "http://maxcdn.bootstrapcdn.com/",
+)
+
+CSP_IMG_SRC = (
+    "'self'",
+
+    ## This might not be necessary. Not having it in place was
+    ## Causing a CSP violation because of an image Lastpass
+    ## injects into form fields. If that's actually the only
+    ## reason we're seeing those violations, than later we can
+    ## remove this exemption again.
+    "data:",
 )
