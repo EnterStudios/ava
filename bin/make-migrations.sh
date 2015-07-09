4f4f7062b1e0399eb916e407e2dab0b6ba93229a
Tom Eastman <tom@eastman.net.nz>
2015-07-10 11:48:50 +1200

    Added a utility script for doing database migrations

---
+++ bin/make-migrations.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## Run 'manage.py makemigrations' on all AVA apps to auto-detect
+## database changes.
+##
+## Uses list-ava-apps.sh to find the apps names.
+
+
+## I don't like having to hardwire this list.
+AVA_APPS="core_auth core_group core_identity core core_project import_ldap test_email test test_twitter"
+
+
+cd $(dirname $0)/..
+
+docker-compose run --rm web ./manage.py makemigrations ${AVA_APPS}
