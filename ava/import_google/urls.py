3b2770c8fde8f3663a902a66ccb425ac15ee3ca5
Laura Bell <ladynerd@Lauras-MacBook-Pro.local>
2015-07-11 19:43:26 +1200

    Many bug fixes
    Flushed out a lot of errors from the app rename that weren’t caught
    earlier

--- ava/import_google/urls.py
+++ ava/import_google/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
 
-from ava.core_google_apps import views
+from ava.import_google import views
 
 urlpatterns = patterns(
     '',
     url(r'^import/$', login_required(views.GoogleDirectoryImport.as_view()),
         name='google-import'),
-    url(r'^auth/$', login_required(views.google_directory_authorize_import.as_view()),
+    url(r'^auth/$', login_required(views.google_directory_authorize_import),
         name='google-auth-import'),
 )
