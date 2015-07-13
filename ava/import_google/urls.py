711ce1dc7dc78a272bc1dfb173a1be4e27c0674d
Laura Bell <ladynerd@Lauras-MacBook-Pro.local>
2015-07-13 21:31:49 +1200

    Update to template and url for google groups

--- ava/import_google/urls.py
+++ ava/import_google/urls.py
@@ -7,10 +7,18 @@ urlpatterns = patterns(
     '',
     url(r'^import/$', login_required(views.GoogleDirectoryImport.as_view()),
         name='google-import'),
+
     url(r'^users/$', login_required(views.GoogleDirectoryUserIndex.as_view()),
         name='google-user-index'),
     url(r'^users/(?P<pk>\d+)/view/$', login_required(views.GoogleDirectoryUserDetail.as_view()),
         name='google-user-detail'),
     url(r'^users/(?P<pk>\d+)/delete/$', login_required(views.GoogleDirectoryUserDelete.as_view()),
         name='google-user-delete'),
+    
+    url(r'^groups/$', login_required(views.GoogleDirectoryGroupIndex.as_view()),
+        name='google-group-index'),
+    url(r'^groups/(?P<pk>\d+)/view/$', login_required(views.GoogleDirectoryGroupDetail.as_view()),
+        name='google-group-detail'),
+    url(r'^groups/(?P<pk>\d+)/delete/$', login_required(views.GoogleDirectoryGroupDelete.as_view()),
+        name='google-group-delete'),
 )
