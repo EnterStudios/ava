ba2197fbca8023683d11993234b43b21e0a92684
Laura Bell <ladynerd@Lauras-MacBook-Pro.local>
2015-07-13 19:49:37 +1200

    Removed unused urls

--- ava/import_google/urls.py
+++ ava/import_google/urls.py
@@ -7,8 +7,4 @@ urlpatterns = patterns(
     '',
     url(r'^import/$', login_required(views.GoogleDirectoryImport.as_view()),
         name='google-import'),
-    url(r'^auth/$', login_required(views.google_directory_authorize_import),
-        name='google-auth-import'),
-    url(r'^auth/return/$', login_required(views.google_directory_auth_return),
-        name='google-auth-return'),
 )
