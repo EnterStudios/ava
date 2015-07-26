d8191546d2da7913cf557dfa3c09a30a2923dcc3
Laura Bell <ladynerd@Lauras-MacBook-Pro.local>
2015-07-25 18:05:14 -0700

    Basics for gathering LDAP stats

--- ava/import_ldap/stats_interface.py
+++ ava/import_ldap/stats_interface.py
@@ -1,11 +1,69 @@
 __author__ = 'ladynerd'
 
-from ava.import_ldap.models import LDAPConfiguration
+from ava.import_ldap.models import LDAPConfiguration, ActiveDirectoryUser
 
-class LDAPStatistics(ldap_configuration):
 
-    LDAP_CONFIG = None
+class LDAPStatistics(LDAPConfiguration):
+    LDAP_CONFIG = LDAPConfiguration
 
-    def __init__(self):
-        LDAP_CONFIG = ldap_configuration
+    DESC = '-'
+    ASC = ''
 
+    def get_stats(self):
+        results = {
+            'never_expires': self.get_never_expires(),
+            'admin_accounts': self.get_admin_accounts(),
+            'never_logged_in': self.get_never_logged_in(),
+            'logon_count': self.get_logon_count(self.DESC),
+            'password_last_set': self.get_password_last_set(self.DESC),
+            'connection_size': self.get_connection_size(self.DESC),
+        }
+        return results
+
+    # Checks the current active directory users for this ldap configuration and returns all users that do not expire
+    def get_never_expires(self):
+        results = []
+        users = ActiveDirectoryUser.objects.filter(ldap_configuration=self.LDAP_CONFIG)
+
+        for user in users:
+            if str.startswith(str(user.account_expires), "9999"):
+                results.append(user)
+
+        return results
+
+    def get_admin_accounts(self):
+        results = []
+        users = ActiveDirectoryUser.objects.filter(ldap_configuration=self.LDAP_CONFIG)
+
+        for user in users:
+            if user.admin_count is "1":
+                results.append(user)
+
+        return results
+
+    def get_never_logged_in(self):
+        results = []
+        users = ActiveDirectoryUser.objects.filter(ldap_configuration=self.LDAP_CONFIG)
+
+        for user in users:
+            if user.logon_count is "":
+                results.append(user)
+
+        return results
+
+    def get_logon_count(self, direction):
+        if direction is self.DESC:
+            return ActiveDirectoryUser.objects.filter(ldap_configuration=self.LDAP_CONFIG).order_by(
+                direction + 'logon_count')
+        else:
+            return ActiveDirectoryUser.objects.filter(ldap_configuration=self.LDAP_CONFIG).order_by('logon_count')
+
+    def get_password_last_set(self, direction):
+        if direction is self.DESC:
+            return ActiveDirectoryUser.objects.filter(ldap_configuration=self.LDAP_CONFIG).order_by(
+                direction + 'password_last_set')
+        else:
+            return ActiveDirectoryUser.objects.filter(ldap_configuration=self.LDAP_CONFIG).order_by('password_last_set')
+
+    def get_connection_size(self, direction):
+        pass
