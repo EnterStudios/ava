8e644a7c7599150a2ca6eca0b1eb651a76b9ad92
Laura Bell <ladynerd@Lauras-MacBook-Pro.local>
2015-07-27 21:12:55 +1200

    Conflict changes

--- ava/dashboard_know/views.py
+++ ava/dashboard_know/views.py
@@ -4,10 +4,6 @@ from django.shortcuts import get_object_or_404
 from django.views import generic
 from ava.import_ldap.models import LDAPConfiguration
 from ava.import_ldap.stats_interface import LDAPStatistics
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/fix_know_dashboard
 
 class KnowDashboardView(generic.ListView):
     template_name = 'know/dashboard.html'
