0aa950fda3b42ca5c5265b3739b60d65187f2ce4
Laura Bell <ladynerd@Lauras-MacBook-Pro.local>
2015-07-27 20:56:16 +1200

    Merge

--- ava/import_ldap/stats_interface.py
+++ ava/import_ldap/stats_interface.py
@@ -4,13 +4,13 @@ __author__ = 'ladynerd'
 
 from ava.import_ldap.models import LDAPConfiguration, ActiveDirectoryUser
 
-class LDAPStatistics(LDAPConfiguration):
-    LDAP_CONFIG = LDAPConfiguration
-
+class LDAPStatistics():
+    LDAP_CONFIG = None
     DESC = '-'
     ASC = ''
 
-    def get_stats(self):
+    def get_stats(self, ldap_config):
+        self.LDAP_CONFIG = ldap_config
         results = {
             'never_expires': self.get_never_expires(),
             'admin_accounts': self.get_admin_accounts(),
@@ -19,6 +19,7 @@ class LDAPStatistics(LDAPConfiguration):
             'password_last_set': self.get_password_last_set(self.DESC),
             'connection_size': self.get_connection_size(self.DESC),
         }
+
         return results
 
     # Checks the current active directory users for this ldap configuration and returns all users that do not expire
@@ -28,8 +29,11 @@ class LDAPStatistics(LDAPConfiguration):
 
         for user in users:
             if str.startswith(str(user.account_expires), "9999"):
+                print("found one : " + user.name + " = " + user.account_expires)
                 results.append(user)
-
+            else:
+                print("ignoring :: " + user.name)
+        print(len(results))
         return results
 
     def get_admin_accounts(self):
@@ -62,9 +66,9 @@ class LDAPStatistics(LDAPConfiguration):
     def get_password_last_set(self, direction):
         if direction is self.DESC:
             return ActiveDirectoryUser.objects.filter(ldap_configuration=self.LDAP_CONFIG).order_by(
-                direction + 'password_last_set')
+                direction + 'pwd_last_set')
         else:
-            return ActiveDirectoryUser.objects.filter(ldap_configuration=self.LDAP_CONFIG).order_by('password_last_set')
+            return ActiveDirectoryUser.objects.filter(ldap_configuration=self.LDAP_CONFIG).order_by('pwd_last_set')
 
     def get_connection_size(self, direction):
         pass
