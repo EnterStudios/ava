f3b58ab57feda2d65c9d4b743cefd18300a01762
Laura Bell <ladynerd@Lauras-MacBook-Pro.local>
2015-07-28 20:05:03 +1200

    Added forms for welcome import

--- ava/import_google/forms.py
+++ ava/import_google/forms.py
@@ -1,23 +1,22 @@
 from django.db import transaction
 from django.forms import ModelForm
-from pip.utils import logging
+import logging
 
 from ava.import_google.models import GoogleConfiguration
 
 log = logging.getLogger(__name__)
 
+
 class GoogleConfigurationForm(ModelForm):
 
     class Meta:
         model = GoogleConfiguration
+        fields = ('domain',)
 
 
-class GoogleConfigurationForm(ModelForm):
-    class Meta:
-        model = GoogleConfiguration
-
-    def run_ldap_import(self):
-        """Run the LDAP Import using the form.
+class GoogleConfigurationWelcomeForm(GoogleConfigurationForm):
+    def run_google_import(self):
+        """Run the Google Import using the form.
 
         We wrap the operation in a transaction, and run the import. If
         the import fails, we roll back the transaction and insert an
@@ -29,10 +28,10 @@ class GoogleConfigurationForm(ModelForm):
         But it saves us a lot of trouble with error-handling during
         the import:
 
-        - We get bounced back to the LDAP wizard step, with a useful
+        - We get bounced back to the Google wizard step, with a useful
           error message.
 
-        - The LDAP configuration is not saved to the database, so
+        - The Google configuration is not saved to the database, so
           we're not left in an inconsistent state, the user can
           simply try again.
 
@@ -43,14 +42,17 @@ class GoogleConfigurationForm(ModelForm):
         """
         try:
             # enter the transaction.
-            with transaction.atomic():
-                # save the form.
-                self.save()
-                # run the LDAP import
-                self.instance.import_all(
-                    user_dn=self.cleaned_data['user_dn'],
-                    user_pw=self.cleaned_data['user_dn'],
-                )
+            # with transaction.atomic():
+            # save the form.
+            # self.save()
+            # run the Google Import
+
+            # This needs love. It should redirect through the google auth flow...
+
+            # self.instance.import_all(
+            #     user_dn=self.cleaned_data['user_dn'],
+            #     user_pw=self.cleaned_data['user_dn'],
+            # )
             # At the moment 'no exception being thrown' is the best
             # measure for success we have, so if we got here, it
             # must have worked.
@@ -60,7 +62,7 @@ class GoogleConfigurationForm(ModelForm):
             # Something went wrong, the transaction has already rolled
             # back. We log the exception, insert an error into the
             # form's errorlist, and trigger the 'invalid_form' view
-            # path to return the customer to the LDAP configuration
+            # path to return the customer to the Google configuration
             # screen.
             log.exception("Error during Google import.", exc_info=e)
             self.add_error(
