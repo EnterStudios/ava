b67cf4780b5f624bcd16e8b49513eb32bdedbc07
Laura Bell <ladynerd@Lauras-MacBook-Pro.local>
2015-07-28 19:29:06 +1200

    Added URL for welcome google import

--- ava/welcome/urls.py
+++ ava/welcome/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns(
     url('^first-user/$', views.CreateFirstUser.as_view(), name='welcome-first-user'),
     url('^import-selection/$', views.ImportSelection.as_view(), name='welcome-import-selection'),
     url('^import-ldap/$', views.ImportLDAP.as_view(), name='welcome-import-ldap'),
+    url('^import-google/$', views.ImportGoogle.as_view(), name='welcome-import-google'),
     url('^import-progress/$', views.ImportProgress.as_view(), name='welcome-import-progress'),
 )
