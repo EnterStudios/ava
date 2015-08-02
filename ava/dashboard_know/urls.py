5b0632cac8d5b20cc19bec6e0d5fcec9a20b4665
Laura Bell <ladynerd@Lauras-MacBook-Pro.local>
2015-08-02 19:14:37 +1200

    New URLs for Google and LDAP know dashboards
    The stats are different for these two integrations so they each need a
    dashboard for their own stuff.

--- ava/dashboard_know/urls.py
+++ ava/dashboard_know/urls.py
@@ -6,5 +6,10 @@ from ava.dashboard_know import views
 urlpatterns = patterns(
     '',
     url(r'^$', login_required(views.KnowDashboardView.as_view()), name='know-dashboard'),
-    url('^(?P<pk>\d+)/$', login_required(views.KnowDashboardView.as_view()), name='know-no-dashboard'),
+    url('^(?P<pk>\d+)/ldap/$', login_required(views.KnowLDAPDashboardView.as_view()), name='know-no-ldap-dashboard'),
+    url('^(?P<pk>\d+)/google/$', login_required(views.KnowGoogleDashboardView.as_view()),
+        name='know-no-google-dashboard'),
+    url('^ldap/$', login_required(views.KnowLDAPDashboardView.as_view()), name='know-ldap-dashboard'),
+    url('^google/$', login_required(views.KnowGoogleDashboardView.as_view()),
+        name='know-google-dashboard'),
 )
