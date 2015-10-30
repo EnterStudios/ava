07ad54fba06ea59ea1143ee438acdeb6708ff876
Sam Bristow <sbristow@dynamiccontrols.com>
2015-10-30 16:07:36 +1300

    Make flake8 happy again

--- ava/core_identity/urls.py
+++ ava/core_identity/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import include, patterns, url
+from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
 from ava.core_auth.decorators import system_admin_required
 
