578f49915d67d80f19702195c4a5ee41f1d7a792
Laura Bell <ladynerd@Lauras-MacBook-Pro.local>
2016-02-02 21:52:56 +1300

    Updated to pull settings from django.conf.settings
    Removing hard coded things and mirroring the style of the google
    integration

--- ava_core/integration/integration_office365/utils.py
+++ ava_core/integration/integration_office365/utils.py
@@ -1,10 +1,11 @@
 # Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See full license at the bottom of this file.
 
-from urllib.parse import quote, urlencode
 import base64
 import json
+from urllib.parse import urlencode
+
 import requests
-from connect.config import client_id, client_secret
+from django.conf import settings
 
 # The URL of the home page of the app. Leave as default if running on localhost:8000.
 home_page_url = 'http://127.0.0.1:8000/'
@@ -18,68 +19,72 @@ authorize_url = '{0}{1}'.format(authority, '/common/oauth2/authorize?{0}')
 # The token issuing endpoint.
 token_url = '{0}{1}'.format(authority, '/common/oauth2/token')
 
+
 # This function creates the signin URL that the app will
 # direct the user to in order to sign in to Office 365 and
 # give the app consent.
-def get_signin_url(redirect_uri):
-  # Build the query parameters for the signin URL.
-  params = { 'client_id': client_id,
-             'redirect_uri': redirect_uri,
-             'response_type': 'code'
-           }
+def get_signin_url():
+    # Build the query parameters for the signin URL.
+    params = {'client_id': settings.OFFICE365_OAUTH2_CLIENT_ID,
+              'redirect_uri': settings.OFFICE365_OAUTH2_REDIRECT_URL,
+              'response_type': 'code'
+              }
 
-  signin_url = authorize_url.format(urlencode(params))
-  return signin_url
+    signin_url = authorize_url.format(urlencode(params))
+    return signin_url
+
 
 def get_signout_url(redirect_uri):
-  params = { 'post_logout_redirect_uri': home_page_url
-           }
+    params = {'post_logout_redirect_uri': home_page_url
+              }
 
-  signout_url = (authority + '/common/oauth2/logout?{0}').format(urlencode(params))
-  return signout_url
+    signout_url = (authority + '/common/oauth2/logout?{0}').format(urlencode(params))
+    return signout_url
+
 
 # This function passes the authorization code to the token
 # issuing endpoint, gets the token, and then returns it.
-def get_token_from_code(auth_code, redirect_uri):
-  # Build the post form for the token request
-  post_data = { 'grant_type': 'authorization_code',
-                'code': auth_code,
-                'redirect_uri': redirect_uri,
-                'client_id': client_id,
-                'client_secret': client_secret,
-                'resource': 'https://graph.microsoft.com'
-              }
+def get_token_from_code(auth_code):
+    # Build the post form for the token request
+    post_data = {'grant_type': 'authorization_code',
+                 'code': auth_code,
+                 'redirect_uri': settings.OFFICE365_OAUTH2_REDIRECT_URL,
+                 'client_id': settings.OFFICE365_OAUTH2_CLIENT_ID,
+                 'client_secret': settings.OFFICE365_OAUTH2_CLIENT_SECRET,
+                 'resource': 'https://graph.microsoft.com'
+                 }
 
-  r = requests.post(token_url, data = post_data)
+    r = requests.post(token_url, data=post_data)
 
-  try:
-    return r.json()
-  except:
-    return 'Error retrieving token: {0} - {1}'.format(r.status_code, r.text)
+    try:
+        return r.json()
+    except:
+        return 'Error retrieving token: {0} - {1}'.format(r.status_code, r.text)
+
 
 # This function takes the access token and breaks it
 # apart to get information about the user.
 def get_user_info_from_token(id_token):
-  # JWT is in three parts, header, token, and signature
-  # separated by '.'.
-  token_parts = id_token.split('.')
-  encoded_token = token_parts[1]
+    # JWT is in three parts, header, token, and signature
+    # separated by '.'.
+    token_parts = id_token.split('.')
+    encoded_token = token_parts[1]
 
-  # Base64 strings should have a length divisible by 4.
-  # If this one doesn't, add the '=' padding to fix it.
-  leftovers = len(encoded_token) % 4
-  if leftovers == 2:
-      encoded_token += '=='
-  elif leftovers == 3:
-      encoded_token += '='
+    # Base64 strings should have a length divisible by 4.
+    # If this one doesn't, add the '=' padding to fix it.
+    leftovers = len(encoded_token) % 4
+    if leftovers == 2:
+        encoded_token += '=='
+    elif leftovers == 3:
+        encoded_token += '='
 
-  # URL-safe base64 decode the token parts.
-  decoded = base64.urlsafe_b64decode(encoded_token.encode('utf-8')).decode('utf-8')
+    # URL-safe base64 decode the token parts.
+    decoded = base64.urlsafe_b64decode(encoded_token.encode('utf-8')).decode('utf-8')
 
-  # Load decoded token into a JSON object.
-  jwt = json.loads(decoded)
+    # Load decoded token into a JSON object.
+    jwt = json.loads(decoded)
 
-  return jwt
+    return jwt
 
 #######################################################################
 #
