f50e98cc4b43f6a8dd16db6923a6e9b06db07a94
Laura Bell <ladynerd@Lauras-MacBook-Pro.local>
2016-02-04 22:17:28 +1300

    Added integration name to utils

--- ava_core/integration/integration_abstract/utils.py
+++ ava_core/integration/integration_abstract/utils.py
@@ -1,3 +1,4 @@
+import json
 import logging
 
 import oauth2client
@@ -26,19 +27,25 @@ def store_credential_in_database(model_name, integration_id, credential):
     try:
         oauth_adapter.credential = credential.to_json()
     except Exception as e:
-        oauth_adapter.credential = str(credential)
+        oauth_adapter.credential = credential
 
     oauth_adapter.save()
 
 
 # retrieve the credential from the database
-def retrieve_credential_from_database(model_name, integration_id):
+def retrieve_credential_from_database(model_name, integration_id, integration_name):
     adapter = apps.get_model(model_name)
-    oauth_adapter = adapter.objects.get(pk=integration_id)
-    credential_json = oauth_adapter.credential
-    if credential_json is None:
-        return None
-    credential = oauth2client.client.OAuth2Credentials.from_json(credential_json)
+    if integration_name is 'google':
+        oauth_adapter = adapter.objects.get(pk=integration_id)
+        credential_json = oauth_adapter.credential
+        if credential_json is None:
+            return None
+        credential = oauth2client.client.OAuth2Credentials.from_json(credential_json)
+    else:
+        integration_adapter = adapter.objects.get(pk=integration_id)
+        log.debug("Credential from database = :: " + str(integration_adapter.credential))
+        credential = json.dumps(obj=integration_adapter.credential)
+        log.debug("Credential after dumps = :: " + str(credential))
     return credential
 
 # retrieve the credential from the database
