9ed9b99728080218fefceebed0c7fc1c0f8f78fa
Laura Bell <ladynerd@Lauras-MacBook-Pro.local>
2016-02-04 22:16:06 +1300

    Modified settings to include office365

--- ava_core/settings/gather.py
+++ ava_core/settings/gather.py
@@ -4,9 +4,13 @@
 GATHER_USE_LOCAL = {
     'google': True,
     'ldap': True,
+    'office365': False,
 }
+
 GATHER_CREATE_LOCAL = {
     'google': False,
     'ldap': False,
+    'office365': True,
 }
+
 GATHER_LOCAL_DATA_LOCATION = '/testdata'
