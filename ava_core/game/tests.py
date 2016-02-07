6b345e7c1949e0a73fb96293c2038559b92deda7
Laura Bell <ladynerd@Lauras-MacBook-Pro.local>
2016-02-07 20:38:32 +1300

    Tests and bug fixes
    Addressed permissions issue on achievement (identified by testing!)

--- ava_core/game/tests.py
+++ ava_core/game/tests.py
@@ -1,78 +1,109 @@
 # Rest Imports
 # Local Imports
+from rest_framework import status
+from rest_framework.reverse import reverse
+from rest_framework.test import APIClient
+
 from ava_core.abstract.test import AvaCoreTest
+from ava_core.game.models import Achievement
 from ava_core.game.test_data import AchievementTestData
 
 
 # Implementation
 class AchievementTest(AvaCoreTest):
-    """
-    Achievement Test
-    """
-
     def setUp(self):
         # Make call to super.
         super(AchievementTest, self).setUp()
+
+        # self.client = APIClient(enforce_csrf_checks=self.enforce_csrf)
+        # response = client.post(self.login_url, self.users['user'], format='json')
+        # token = response.data['token']
+        # print("Token :: " + token)
+        # client.credentials(HTTP_AUTHORIZATION='JWT ' + token)
 
         # Set the data type.
         self.data = AchievementTestData()
 
     def test_achievement_create_as_user(self):
-        pass
+        url = reverse('achievement-list')
+        data = self.data.standard
+
+        self.login_user(user='normal')
+
+        response = self.client.post(url, data, format='json')
+        self.assertIn(response.status_code, self.status_forbidden)
+        self.assertEqual(Achievement.objects.count(), 0)
 
     def test_achievement_create_as_admin(self):
-        pass
+        url = reverse('achievement-list')
+        data = self.data.standard
+
+        self.login_user(user='admin')
+
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Achievement.objects.count(), 1)
+        self.assertEqual(Achievement.objects.get().name, data['name'])
 
     def test_achievement_create_as_unauthenticated(self):
-        pass
+        url = reverse('achievement-list')
+        data = self.data.standard
+
+        self.logout_user()
+
+        response = self.client.post(url, data, format='json')
+
+        self.assertIn(response.status_code, self.status_forbidden)
+        self.assertEqual(Achievement.objects.count(), 0)
 
     def test_achievement_retrieve_single_as_user(self):
-        pass
+        self.assertEqual(1,"Test not written")
 
     def test_achievement_retrieve_all_as_user(self):
-        pass
+        self.assertEqual(1,"Test not written")
 
     def test_achievement_retrieve_single_as_admin(self):
-        pass
+        self.assertEqual(1,"Test not written")
 
     def test_achievement_retrieve_all_as_admin(self):
-        pass
+        self.assertEqual(1,"Test not written")
 
     def test_achievement_retrieve_single_as_unauthorized(self):
-        pass
+        self.assertEqual(1,"Test not written")
 
     def test_achievement_retrieve_all_as_unauthorized(self):
-        pass
+        self.assertEqual(1,"Test not written")
 
     def test_achievement_update_exists_as_user(self):
-        pass
+        self.assertEqual(1,"Test not written")
 
     def test_achievement_update_does_not_exist_as_user(self):
-        pass
+        self.assertEqual(1,"Test not written")
 
     def test_achievement_update_exists_as_admin(self):
-        pass
+        self.assertEqual(1,"Test not written")
 
     def test_achievement_update_does_not_exist_as_admin(self):
-        pass
+        self.assertEqual(1,"Test not written")
 
     def test_achievement_update_exists_as_unauthorized(self):
-        pass
+        self.assertEqual(1,"Test not written")
 
     def test_achievement_update_does_not_exist_as_unauthorized(self):
-        pass
+        self.assertEqual(1,"Test not written")
 
     def test_achievement_delete_does_not_exist_as_user(self):
-        pass
+        self.assertEqual(1,"Test not written")
 
     def test_achievement_delete_exists_as_admin(self):
-        pass
+        self.assertEqual(1,"Test not written")
 
     def test_achievement_delete_does_not_exist_as_admin(self):
-        pass
+        self.assertEqual(1,"Test not written")
 
     def test_achievement_delete_exists_as_unauthorized(self):
-        pass
+        self.assertEqual(1,"Test not written")
 
     def test_achievement_delete_does_not_exist_as_unauthorized(self):
-        pass
+        self.assertEqual(1,"Test not written")
--- ava_core/game/views.py
+++ ava_core/game/views.py
@@ -1,9 +1,9 @@
 import logging
 
-from ava_core.abstract.permissions import IsRetrieveOnly
 from rest_framework import permissions
 from rest_framework import viewsets
 
+from ava_core.abstract.permissions import IsAdminOrReadOnly
 from .models import Achievement
 from .serializers import AchievementSerializer
 
@@ -14,8 +14,8 @@ class AchievementAPI(viewsets.ModelViewSet):
     queryset = Achievement.objects.none()  # Required for DjangoModelPermissions
     serializer_class = AchievementSerializer
     permission_classes = (permissions.IsAuthenticated,
-                          permissions.IsAdminUser,
-                          IsRetrieveOnly)
+                          IsAdminOrReadOnly,
+                          )
 
     def get_queryset(self):
         return Achievement.objects.all()
