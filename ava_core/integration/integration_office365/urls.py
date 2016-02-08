506de4428a558df862e63126777891f7311baa6d
Laura Bell <ladynerd@Lauras-MacBook-Pro.local>
2016-02-08 21:37:56 +1300

    Added base name to url so that reverse works

--- ava_core/integration/integration_office365/urls.py
+++ ava_core/integration/integration_office365/urls.py
@@ -4,15 +4,11 @@ from rest_framework.routers import DefaultRouter
 from .views import redirect, callback, Office365AdapterAPI
 
 router = DefaultRouter()
-router.register(r'setup', Office365AdapterAPI)
+router.register(r'setup', Office365AdapterAPI, "office365-setup")
 
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^redirect/(?P<pk>[0-9]+)/$', redirect),
     url(r'^callback/$', callback),
-    # url(r'^home/$', views.home, name='home'),
-    # url(r'^get_token/$', get_token, name='get_token'),
-    # url(r'^main/$', views.main, name='main'),
-    # url(r'^send_mail/$', views.send_mail, name='send_mail'),
-    # url(r'^disconnect/$', views.disconnect, name='disconnect')
+
 ]
